ad82c211c0456e08d307dd038b9381fdd91c0c52
Rico Yao <rico@box.com>
2016-04-14 20:03:51 -0700

    Update MWZoomingScrollView.m

--- Pod/Classes/MWZoomingScrollView.m
+++ Pod/Classes/MWZoomingScrollView.m
@@ -229,11 +229,11 @@
         CGFloat xScale = boundsSize.width / imageSize.width;    // the scale needed to perfectly fit the image width-wise
         CGFloat yScale = boundsSize.height / imageSize.height;  // the scale needed to perfectly fit the image height-wise
         // Zooms standard portrait images on a 3.5in screen but not on a 4in screen.
-        if (ABS(boundsAR - imageAR) < 0.17) {
-            zoomScale = MAX(xScale, yScale);
-            // Ensure we don't zoom in or out too far, just in case
-            zoomScale = MIN(MAX(self.minimumZoomScale, zoomScale), self.maximumZoomScale);
-        }
+//        if (ABS(boundsAR - imageAR) < 0.17) {
+//            zoomScale = MAX(xScale, yScale);
+//            // Ensure we don't zoom in or out too far, just in case
+//            zoomScale = MIN(MAX(self.minimumZoomScale, zoomScale), self.maximumZoomScale);
+//        }
     }
     return zoomScale;
 }
